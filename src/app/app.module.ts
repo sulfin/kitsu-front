7d1278313da8ae57ed0b4f5842288c9ce9618847
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-01-06 14:33:00 +0100

    toutes jolies les couleurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { NavBarreComponent } from './nav-barre/nav-barre.component';
 import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 import { PageSortiesComponent } from './page-sorties/page-sorties.component';
 import { PageAnimeComponent } from './page-anime/page-anime.component';
-import { PagePersonnagesComponent } from './page-personnages/page-personnages.component';
 import { PageSaisonComponent } from './page-saison/page-saison.component';
 import { PageAboutComponent } from './page-about/page-about.component';
 import { FooterComponent } from './footer/footer.component';
@@ -29,7 +28,6 @@ import { CategoriesSaisonComponent } from './categories-saison/categories-saison
     PageAccueilComponent,
     PageSortiesComponent,
     PageAnimeComponent,
-    PagePersonnagesComponent,
     PageSaisonComponent,
     PageAboutComponent,
     FooterComponent,
